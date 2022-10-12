b7a1187b413828161aaede61a15b5f2efc5db65f
RuanvanJaarsveld <rexvjaarsveld@gmail.com>
2022-10-12 14:45:06 +0200

    Added Exercise 10
    Finished the 6th Section of the course and completed the first exercise question for the Arrays and Lists part of the course.

---
+++ CSharp Courses/CSharp Fundamentals Exercise 10/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp_Fundamentals_Exercise_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Init
+            var userInput = "Begin";
+            var names = new List<String>();
+
+            //Input
+            do
+            {
+                Console.WriteLine("Please enter a name!");
+                userInput = Console.ReadLine();
+                if(userInput != "")
+                {
+                    names.Add(userInput);
+                }
+            } while (userInput != "");
+
+            //Processing
+            //Output
+            if (names.Count == 0)
+            {
+                Console.WriteLine("");
+            }
+            else if (names.Count == 1)
+            {
+                Console.WriteLine(names[0] + " likes your post.");
+            }
+            else if (names.Count == 2)
+            {
+                Console.WriteLine(names[0] + " and " + names[1] + " like your post.");
+            }
+            else if (names.Count >= 3)
+            {
+                Console.WriteLine(names[0] + ", " + names[1] + " and " + (names.Count - 2) + " others like your post.");
+            }        
+
+            //Debug
+            /*for(var i = 0; i<names.Count; i++)
+            {
+                Console.WriteLine(names[i]);
+            }*/
+        }
+    }
+}
