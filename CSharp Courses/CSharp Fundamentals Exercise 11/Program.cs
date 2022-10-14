55ebcbc6f8044555ca0ec31d268829c4bc6967bd
RuanvanJaarsveld <rexvjaarsveld@gmail.com>
2022-10-14 03:06:18 +0200

    Added Exercise 11
    Further small progress today just to keep the ball rolling

---
+++ CSharp Courses/CSharp Fundamentals Exercise 11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSharp_Fundamentals_Exercise_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a name.");
+            var userInput = Console.ReadLine();
+            char[] nameStart = new char[userInput.Length];
+            String nameEnd = "";
+            var counter = 0;
+
+            foreach (char letter in userInput)
+            {                
+                nameStart[userInput.Length - counter - 1] = letter;
+                counter++;
+                //Debug
+                //Console.WriteLine(letter);
+            }
+
+            foreach (char letter in nameStart)
+            {
+                nameEnd = nameEnd + letter;
+            }
+            
+
+            Console.WriteLine(nameEnd);
+        }
+    }
+}
